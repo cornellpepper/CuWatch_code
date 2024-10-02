320f0188173922f73b07cb2023e85e398ac2f8b3
Peter Wittich <wittich@cornell.edu>
2024-10-02 17:11:23 -0400

    Update readout.py

--- src/readout.py
+++ src/readout.py
@@ -8,6 +8,7 @@ import sdcard
 import urequests
 import ujson
 import sys
+import logging
 
 
 # Init Wi-Fi Interface
@@ -21,7 +22,7 @@ def init_wifi(ssid, password):
     else:
         wlan.connect(ssid, password)
     # Wait for Wi-Fi connection
-    connection_timeout = 10
+    connection_timeout = 30
     while connection_timeout > 0:
         print(wlan.status())
         if wlan.status() >= 3:
@@ -88,6 +89,15 @@ def unmount_sdcard():
     uos.umount("/sd")
     print("SD card unmounted.")
 
+def init_logging(filename):
+    """initialize logging with the specified filename"""
+    logging.basicConfig(filename=filename, 
+                        level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger()
+    print(f"Logging to file {filename}")
+    return logger
+
 
 def calibrate_average_rms(adc, n):
     """calibrate the threshold value by taking the average of n samples. Assumes no muons are present"""
@@ -97,15 +107,31 @@ def calibrate_average_rms(adc, n):
         value = adc.read_u16()
         sum += value
         sum_squared += value ** 2
+        led2.toggle()
         time.sleep_ms(10) # wait 10 ms
     mean = sum / n
     variance = (sum_squared / n) - (mean ** 2)
     standard_deviation = variance ** 0.5
+    led2.value(1)
     return mean, standard_deviation
+
+
+def usr_switch_pressed(pin):
+    """interrupt handler for the user switch"""
+    global switch_pressed
+    if pin.value() == 1:
+        switch_pressed = True
 
 ### end of function definitions
 
-init_wifi("SSID", "PASSWORD")
+
+if (not init_wifi("RedRover", None) ):
+    print("Couldn't initialize wifi")
+    while True:
+        led2.toggle()
+        led1.toggle()
+        time.sleep(1)
+        
 now = init_RTC()
 print(f"current time is {now}")
 
@@ -113,9 +139,21 @@ sd = init_sdcard()
 
 # Set up ADC on Pin 26 (GP26)
 adc = ADC(Pin(26))  # Pin 26 is GP26, which is ADC0
+temp = ADC(Pin(27))
 
-threshold = 13050 # random value for now
-reset_threshold = 12900 # random 
+# setup LEDs
+led1 = Pin('LED', Pin.OUT)
+led2 = Pin(15, Pin.OUT) # local LED on pepper carrier board
+led1.value(0)
+led2.value(0)
+
+# set up switch on pin 16
+usr_switch = Pin(16, Pin.IN, Pin.PULL_DOWN)
+print(f"switch state at start is {usr_switch.value()}")
+switch_pressed = False
+usr_switch.irq(trigger=Pin.IRQ_RISING, handler=usr_switch_pressed)
+
+
 
 muon_count = 0
 
@@ -128,14 +166,24 @@ try:
     day = now[2]
     hour = now[3]
     minute = now[4]
-    filename = f"/sd/muon_data_{year}{month:02d}{day:02d}_{hour:02d}{minute:02d}.csv"
+    suffix = f"{year}{month:02d}{day:02d}_{hour:02d}{minute:02d}"
+    # data file
+    filename = f"/sd/muon_data_{suffix}.csv"
+    # log file
+    logger = init_logging(f"/sd/log_{suffix}.txt")
+
     # calibrate the adc
     print("calibrating")
+    led2.value(1)
     baseline, rms = calibrate_average_rms(adc, 250)
-    threshold = baseline + 3.2*rms
-    reset_threshold = baseline - 3*rms
-    print("calibrated threshold", threshold, reset_threshold)
+    print(f"baseline is {baseline:.1f} pm {rms:.1f}")
+    #threshold = baseline + 3.2*rms
+    threshold = baseline + 500
+    reset_threshold = baseline +  50
+    print("Thresholds", threshold, reset_threshold)
+    logger.info(f"calibrated threshold {threshold} and reset threshold {reset_threshold}")
     time.sleep(5)
+    led2.value(0)
     print(f"writing to {filename}")
     with open(filename, "w") as f:
         f.write("Muon Count, ADC Value, dt, time\n")
@@ -144,6 +192,7 @@ try:
         start_time = time.ticks_ms()
         end_time = start_time
         while True:
+            led1.toggle()
             adc_value = adc.read_u16()  # Read the ADC value (0 - 65535)
             #print(adc_value)
             if ( adc_value > threshold ) :
@@ -159,6 +208,7 @@ try:
                 start_time = end_time
                 wait_counts = 0
                 print(adc_value, dt)
+                logger.info(f"muon count {muon_count}, adc value {adc_value}, dt {dt}, time {end_time}")
                 # wait to drop beneath reset threshold
                 time.sleep_ms(10)
                 while ( adc.read_u16() > reset_threshold ):
@@ -166,14 +216,22 @@ try:
                     time.sleep_ms(10)
                     if ( wait_counts > 1000 ):
                         print("waited too long", adc.read_u16())
+                        logger.warning(f"waited too long, adc value {adc.read_u16()}")
                         break
                     #print(adc.read_u16())
                 print("wait counts", wait_counts)
+                logger.info(f"wait counts {wait_counts}")  
                 # sync the file
                 f.flush()
+            if usr_switch_pressed:
+                print("switch pressed, exiting")
+                logger.info("switch pressed, exiting")
+                break
 finally:
     unmount_sdcard()
     print("muon count", muon_count)
     print(f"thresholds were {threshold} and {reset_threshold}")
     print(f"ending time is {time.localtime()}")
     print(f"data written to {filename}")
+    logger.info(f"muon count {muon_count}, thresholds {threshold} and {reset_threshold}, ending time {time.localtime()}")
+    logging.shutdown()
