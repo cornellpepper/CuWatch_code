f2fefb9b62a7bf6be90eacadaf70c090523b471e
Peter Wittich <wittich@cornell.edu>
2025-09-04 13:48:20 -0400

    remove microdot for v5

--- src/install_mqtt.sh
+++ src/install_mqtt.sh
@@ -21,30 +21,3 @@ mpremote mip install umqtt.simple
 mpremote fs cp $FILES :
 mpremote fs cp $MAIN_FILE :main.py
 
-# download the microdot library if it does not exist
-MICRODOT_VER=2.0.6
-MICRODOT_TGZ="https://github.com/miguelgrinberg/microdot/archive/refs/tags/v"${MICRODOT_VER}".tar.gz"
-
-MICRODOT_FILES="microdot.py \
-                __init__.py"
-MICRODOT_DIR="microdot-"${MICRODOT_VER}
-if [ ! -d $MICRODOT_DIR ]; then
-    curl -L -O $MICRODOT_TGZ
-    if [ $? -ne 0 ]; then
-        echo "Failed to download microdot library"
-        exit 1
-    fi
-    tar -xzf v${MICRODOT_VER}.tar.gz
-    if [ $? -ne 0 ]; then
-        echo "Failed to extract microdot library"
-        exit 1
-    fi
-fi
-
-cd $MICRODOT_DIR/src/microdot
-
-for f in $MICRODOT_FILES; do
-    mpy-cross $f
-    ff=$(basename $f .py).mpy 
-    mpremote fs cp $ff :
-done
